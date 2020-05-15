c2a455939ede4de1b13f6d2ad0a575cb2008b9e7
qleclerc <quentin.leclerc@lshtm.ac.uk>
2020-05-15 13:04:26 +0200

    update data

--- scripts/current_deaths.R
+++ scripts/current_deaths.R
@@ -1,8 +1,8 @@
 ## This file is generated automatically by `clean_latest_online_deaths`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2020-05-13 19:40:35
+## This file was last updated on the: 2020-05-15 13:04:12
 
 current_deaths <- here::here("data",
                                  "rds",
-                                 "deaths_2020-05-12.rds")
+                                 "deaths_2020-05-14.rds")
--- scripts/current_pathways.R
+++ scripts/current_pathways.R
@@ -1,8 +1,8 @@
 ## This file is generated automatically by `clean_latest_pathways`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2020-05-13 20:09:49
+## This file was last updated on the: 2020-05-15 12:59:22
 
 current_pathways <- here::here("data",
                                  "rds",
-                                 "pathways_2020-05-12.rds")
+                                 "pathways_2020-05-14.rds")
