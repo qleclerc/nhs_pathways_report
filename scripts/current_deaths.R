207a1422760f1a8ab814f220e40f5bf41424bcca
qleclerc <quentin.leclerc@lshtm.ac.uk>
2020-05-19 11:05:00 +0200

    update 19/05

--- scripts/current_deaths.R
+++ scripts/current_deaths.R
@@ -1,8 +1,8 @@
 ## This file is generated automatically by `clean_latest_online_deaths`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2020-05-18 11:00:41
+## This file was last updated on the: 2020-05-19 10:51:56
 
 current_deaths <- here::here("data",
                                  "rds",
-                                 "deaths_2020-05-17.rds")
+                                 "deaths_2020-05-18.rds")
