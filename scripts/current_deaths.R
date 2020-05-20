5a9c6d7f6c34d662f498e25fca950e6595452d60
qleclerc <quentin.leclerc@lshtm.ac.uk>
2020-05-20 18:11:16 +0200

    update latest version, fix github actions bug

--- scripts/current_deaths.R
+++ scripts/current_deaths.R
@@ -1,12 +1,8 @@
 ## This file is generated automatically by `clean_latest_online_deaths`
 ## Do not edit it by hand!
 
-<<<<<<< HEAD
-## This file was last updated on the: 2020-05-18 10:08:35
-=======
-## This file was last updated on the: 2020-05-19 10:51:56
->>>>>>> clean_report
+## This file was last updated on the: 2020-05-20 17:53:23
 
 current_deaths <- here::here("data",
                                  "rds",
-                                 "deaths_2020-05-18.rds")
+                                 "deaths_2020-05-20.rds")
--- scripts/current_pathways.R
+++ scripts/current_pathways.R
@@ -1,12 +1,8 @@
 ## This file is generated automatically by `clean_latest_pathways`
 ## Do not edit it by hand!
 
-<<<<<<< HEAD
-## This file was last updated on the: 2020-05-18 10:08:34
-=======
-## This file was last updated on the: 2020-05-19 10:52:50
->>>>>>> clean_report
+## This file was last updated on the: 2020-05-20 17:55:25
 
 current_pathways <- here::here("data",
                                  "rds",
-                                 "pathways_2020-05-18.rds")
+                                 "pathways_2020-05-19.rds")
