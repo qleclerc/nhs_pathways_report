8f0ef085839e61e7c7f89f4d0f9e005a3c42eb09
qleclerc <quentin.leclerc@lshtm.ac.uk>
2020-06-02 17:01:55 +0200

    update nhs pathways data formatting

--- scripts/current_pathways.R
+++ scripts/current_pathways.R
@@ -1,7 +1,7 @@
 ## This file is generated automatically by `clean_latest_pathways`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2020-06-02 13:53:16
+## This file was last updated on the: 2020-06-02 16:55:02
 
 current_pathways <- here::here("data",
                                  "rds",
