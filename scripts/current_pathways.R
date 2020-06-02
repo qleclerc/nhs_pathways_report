bbcba8660c061c06b7fdc9f48598bd65567405b7
qleclerc <quentin.leclerc@lshtm.ac.uk>
2020-06-02 14:03:04 +0200

    manual update 06/02, fix deaths guess_dates

--- scripts/current_pathways.R
+++ scripts/current_pathways.R
@@ -1,8 +1,8 @@
 ## This file is generated automatically by `clean_latest_pathways`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2020-06-01 10:25:55
+## This file was last updated on the: 2020-06-02 13:53:16
 
 current_pathways <- here::here("data",
                                  "rds",
-                                 "pathways_2020-05-31.rds")
+                                 "pathways_2020-06-01.rds")
