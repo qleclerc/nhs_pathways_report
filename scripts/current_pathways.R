8d3d8bd1d044b44fada07485067b3feb9cf47aa3
GitHub Action <action@github.com>
2021-03-21 10:47:34 +0000

    Auto-updated: new NHS pathways and death data

--- scripts/current_pathways.R
+++ scripts/current_pathways.R
@@ -1,7 +1,7 @@
 ## This file is generated automatically by `clean_latest_pathways`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2021-03-20 10:30:03
+## This file was last updated on the: 2021-03-21 10:31:15
 
 current_pathways <- here::here("data",
                                  "rds",
